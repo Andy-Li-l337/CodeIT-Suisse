732efd258ddd0368bc4d6e4ed3eba8d747c2a925
DevilsWings <memosgame@gmail.com>
2021-09-26 03:43:13 +0800

    Renamed Module

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -4,4 +4,4 @@ people = {}
 import codeitsuisse.routes.fixedrace
 import codeitsuisse.routes.optopt
 import codeitsuisse.routes.square
-import codeitsuisse.routes.ciphercracking
+import codeitsuisse.routes.crack
