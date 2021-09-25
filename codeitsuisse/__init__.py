37bc4982335f3d8bbbd4b65b7d0ff1ec2c9720e3
DevilsWings <memosgame@gmail.com>
2021-09-26 03:32:28 +0800

    Disabled Ciphercracking

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,8 +1,7 @@
-
 from flask import Flask
 app = Flask(__name__)
 people = {}
-import codeitsuisse.routes.ciphercracking
+#import codeitsuisse.routes.ciphercracking
 import codeitsuisse.routes.fixedrace
 import codeitsuisse.routes.optopt
 import codeitsuisse.routes.square
