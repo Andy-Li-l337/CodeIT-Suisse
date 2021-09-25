8121f0a1141f38e3976cbec75ede361d46f631f8
DevilsWings <memosgame@gmail.com>
2021-09-26 04:47:46 +0800

    Updated Crack

--- codeitsuisse/routes/crack.py
+++ codeitsuisse/routes/crack.py
@@ -9,14 +9,17 @@ logger = logging.getLogger(__name__)
 @app.route('/cipher-cracking', methods=['POST'])
 def crackCode():
     data = request.get_json()
+    answer = "NULL"
+    logger.info(data)
     for i in data:
         if i['est_mins'] < 0.5:
-            f_x = math.ceil(math.log(2*i['X']-1)*1000)/1000
-            for k in range(10**i['D']):
-                encodingstr = f"{k}::{f_x}"
-                shaed_data = hashlib.sha256(
-                    encodingstr.encode('utf-8')).hexdigest()
-                if str(shaed_data) == i['Y']:
-                    print(k)
-                    break
-    return data
+            f_x = math.ceil(math.log(int(i['X'])-1)*1000)/1000
+            for k in range(10**(i['D'])):
+                for g in range(1000000):
+                    encodingstr = f"{k}::{g//1000}.{g%1000:0>3}"
+                    shaed_data = hashlib.sha256(
+                        encodingstr.encode('utf-8')).hexdigest()
+                    if str(shaed_data) == str(i['Y']):
+                        print("SOLUTION:", k, g/1000)
+                        return str(data)
+    return str(data)
