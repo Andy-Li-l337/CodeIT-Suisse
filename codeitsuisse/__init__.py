82de1c1b8c41e612869cf193f637d0080b4a25c7
DevilsWings <memosgame@gmail.com>
2021-09-26 04:42:08 +0800

    Fixed Init

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,8 +1,7 @@
-import codeitsuisse.routes.crack
-import codeitsuisse.routes.square
-import codeitsuisse.routes.optopt
-import codeitsuisse.routes.fixedrace
 from flask import Flask
 app = Flask(__name__)
-people = {'Karina Kuder': 2, 'Lamont Lasch': 1, 'Jared Jinkins': 1, 'Fabian Fogel': 1, 'Spring Sawin': 1, 'Yuette Yurick': 1, 'Leana Lynde': 1, 'Tracie Temblador': 1, 'Ernesto Eno': 1, 'Percy Parisi': 1,
-          'Jefferson Juhl': 1, 'Olympia Oliphant': 1, 'Sharyl Shepler': 1, 'Alfonso Allred': 1, 'Simon Sprayberry': 1, 'Lauretta Lippard': 1, 'Rudolf Ravelo': 1, 'Astrid Acheson': 1, 'Pamula Parrinello': 1}
+people = {'Karina Kuder': 2, 'Lamont Lasch': 1, 'Jared Jinkins': 1, 'Fabian Fogel': 1, 'Spring Sawin': 1, 'Yuette Yurick': 1, 'Leana Lynde': 1, 'Tracie Temblador': 1, 'Ernesto Eno': 1, 'Percy Parisi': 1, 'Jefferson Juhl': 1, 'Olympia Oliphant': 1, 'Sharyl Shepler': 1, 'Alfonso Allred': 1, 'Simon Sprayberry': 1, 'Lauretta Lippard': 1, 'Rudolf Ravelo': 1, 'Astrid Acheson': 1, 'Pamula Parrinello': 1}
+import codeitsuisse.routes.fixedrace
+import codeitsuisse.routes.optopt
+import codeitsuisse.routes.square
+import codeitsuisse.routes.crack
