8307e98fce3697034166b0d21edc68515703ebd8
DevilsWings <memosgame@gmail.com>
2021-09-26 03:40:29 +0800

    Rearranged

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 people = {}
-from codeitsuisse.routes import *
+import codeitsuisse.routes.fixedrace
+import codeitsuisse.routes.optopt
+import codeitsuisse.routes.square
+import codeitsuisse.routes.ciphercracking
