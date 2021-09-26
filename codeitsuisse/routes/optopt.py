d4f3648710423fd1662a6bc95e9ddc0e0e980b97
DevilsWings <memosgame@gmail.com>
2021-09-26 09:08:22 +0800

    Updated optopt

--- codeitsuisse/routes/optopt.py
+++ codeitsuisse/routes/optopt.py
@@ -16,6 +16,7 @@ def calculate():
     logger.info(data)
     options, gauss = data.get("options"), data.get("view")
     result = solve(options, gauss)
+    logger.info(result)
     return json.dumps(result)
 
 
@@ -23,11 +24,12 @@ def solve(options, gauss):
     gaussians = [truncnorm((i['min'] - i['mean']) / math.sqrt(i['var']), (i['max'] - i['mean']) /
                            math.sqrt(i['var']), loc=i['mean'], scale=math.sqrt(i['var'])) for i in gauss]
     rv = gaussians[0]
-    x = np.linspace(rv.ppf(0.01), rv.ppf(0.99), 100)
+    x = np.linspace(rv.ppf(0.0001), rv.ppf(0.9999), 1000)
     returns = [np.sum(np.multiply(rv.pdf(x), np.where(x < j['strike'], -j['premium'], x-(j['strike']+j['premium'])))) if j["type"] ==
                "call" else np.sum(np.multiply(rv.pdf(x), np.where(x > j['strike'], -j['premium'], (j['strike']-j['premium'])-x))) for j in options]
+    ans = [0] * len(returns)
     if max(returns) >= abs(min(returns)):
-        ans = [100 if i == max(returns) else 0 for i in returns]
+        ans[returns.index(max(returns))] = 100
     else:
-        ans = [-100 if i == min(returns) else 0 for i in returns]
+        ans[returns.index(min(returns))] = -100
     return ans
