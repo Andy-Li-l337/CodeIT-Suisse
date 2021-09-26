53f1b95a3d7779c0f6b7b18624d6b6330ca3675a
DevilsWings <memosgame@gmail.com>
2021-09-26 10:00:17 +0800

    Updated opt

--- codeitsuisse/routes/optopt.py
+++ codeitsuisse/routes/optopt.py
@@ -22,14 +22,21 @@ def calculate():
 
 def solve(options, gauss):
     gaussians = [truncnorm((i['min'] - i['mean']) / math.sqrt(i['var']), (i['max'] - i['mean']) /
-                           math.sqrt(i['var']), loc=i['mean'], scale=math.sqrt(i['var'])) for i in gauss]
-    rv = gaussians[0]
+                           math.sqrt(i['var']), loc=i['mean'], scale=math.sqrt(i['var'])), i['weight'] for i in gauss]
+    rv = gaussians[0][0]
     x = np.linspace(rv.ppf(0.0001), rv.ppf(0.9999), 1000)
-    returns = [np.sum(np.multiply(rv.pdf(x), np.where(x < j['strike'], -j['premium'], x-(j['strike']+j['premium'])))) if j["type"] ==
-               "call" else np.sum(np.multiply(rv.pdf(x), np.where(x > j['strike'], -j['premium'], (j['strike']-j['premium'])-x))) for j in options]
+    returns = [np.sum(np.multiply(averageStockProbability(gaussians, x), np.where(x < j['strike'], -j['premium'], x-(j['strike']+j['premium'])))) if j["type"] ==
+               "call" else np.sum(np.multiply(averageStockProbability(gaussians, x), np.where(x > j['strike'], -j['premium'], (j['strike']-j['premium'])-x))) for j in options]
     ans = [0] * len(returns)
     if max(returns) >= abs(min(returns)):
         ans[returns.index(max(returns))] = 100
     else:
         ans[returns.index(min(returns))] = -100
     return ans
+
+
+def averageStockProbability(gaussians, x):
+    total = 0
+    for _, w in gaussians:
+        total += w
+    return sum([np.multiply(i.pdf(x), weight)/total for i, weight in gaussians])
