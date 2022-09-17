3d9629b2968561ceea55133b8ed774e8ff0dbec3
Andy-Li-l337 <cheukhinlch@gmail.com>
2022-09-17 18:45:16 +0800

    Updated Stonks receiver

--- codeitsuisse/routes/stonks.py
+++ codeitsuisse/routes/stonks.py
@@ -4,13 +4,15 @@ from flask import request, jsonify
 from codeitsuisse import app
 logger = logging.getLogger(__name__)
 @app.route("/%20stonks", methods=['POST'])
-def sol():
+@app.route("/ stonks", methods=['POST'])
+def stoneSolve():
     for case in request.get_json():
         energy = case['energy']
         print(f'{energy=}')
         captial = case['capital']
         print(f'{captial=}')
-        timeline = {name:(val['price'],val['qty']) for name,val in case['timeline'].items()}
+        timeline = { year:{name:(val['price'],val['qty']) for name,val in com.items()} for year,com in case['timeline'].items()}
         for i,j in timeline.items():
             print(i, j)
         print("-"*30)
+    return jsonify({})
