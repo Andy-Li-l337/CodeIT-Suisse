e4b34e69c08f16fb422a81a5fec6ad92028d8689
Andy-Li-l337 <87371249+Andy-Li-l337@users.noreply.github.com>
2022-09-17 10:47:15 +0800

    Updated Solution for correctness

--- codeitsuisse/routes/collatz.py
+++ codeitsuisse/routes/collatz.py
@@ -9,16 +9,19 @@ def naive():
     logger.info(request.get_json())
     #print(request.get_json())
     output = []
+    lookupDict = {1:4}
     for inputList in request.get_json():
-        outputList = []
         for elem in inputList:
-            maxValue = 1
-            while elem != 1:
-                if elem % 2:
-                    elem = 3*elem + 1
-                else:
-                    elem //= 2
-                maxValue = max(elem, maxValue)
-            outputList.append(maxValue)
-        output.append(outputList)
+            if elem not in lookupDict:
+                maxValue = elem
+                newElem = elem
+                while newElem not in lookupDict:
+                    if newElem % 2:
+                        newElem = 3*newElem + 1
+                    else:
+                        newElem //= 2
+                    maxValue = max(newElem, maxValue)
+                # logger.info(elem)
+                lookupDict[elem] = max(maxValue,lookupDict[newElem])
+        output.append([lookupDict[i] for i in inputList])
     return jsonify(output)
