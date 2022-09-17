6cbd98c3b982205374fcb369ec623952e0b4e5a8
Andy-Li-l337 <cheukhinlch@gmail.com>
2022-09-17 18:22:54 +0800

    stonks check

--- codeitsuisse/routes/stonks.py
+++ codeitsuisse/routes/stonks.py
@@ -3,6 +3,14 @@ import json
 from flask import request, jsonify
 from codeitsuisse import app
 logger = logging.getLogger(__name__)
-@app.route("/%20cryptocollapz", methods=['POST'])
+@app.route("/stonks", methods=['POST'])
 def sol():
-    pass
+    for case in request.get_json():
+        energy = case['energy']
+        print(f'{energy=}')
+        captial = case['capital']
+        print(f'{captial=}')
+        timeline = {name:(val['price'],val['qty']) for name,val in case['timeline'].items()}
+        for i,j in timeline.items():
+            print(i, j)
+        print("-"*30)
