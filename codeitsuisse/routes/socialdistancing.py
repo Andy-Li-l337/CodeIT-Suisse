aef8838f2348f1ec9d7d17811aa1de5f671794e8
Andy-Li-l337 <cheukhinlch@gmail.com>
2022-09-17 23:35:43 +0800

    Allow POST method for social distancing

--- codeitsuisse/routes/socialdistancing.py
+++ codeitsuisse/routes/socialdistancing.py
@@ -4,7 +4,7 @@ import os
 from flask import request, jsonify
 from codeitsuisse import app
 
-@app.route("/social-distancing")
-@app.route("/ /social-distancing")
+@app.route("/social-distancing", methods=["POST"])
+@app.route("/ /social-distancing", methods=["POST"])
 def socialDistancePermutation():
     return jsonify(["No Solution" for i in request.get_json()])
